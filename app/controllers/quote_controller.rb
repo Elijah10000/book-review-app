e46dd9591d755aeb59ed4bd5a96ab6f426f4ca55
Elijah2609 <56544041+Elijah2609@users.noreply.github.com>
2022-12-05 19:38:26 +0000

    fimished-api

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -1,6 +1,6 @@
 class QuoteController < ApplicationController
     def index
-        response = HTTParty.get('https://quotes.rest/qod.json')
+        response = HTTParty.get('https://api.api-ninjas.com/v1/quotes?category=education', :headers => { 'content-type': 'application/json', 'X-API-Key': 'TNxBtDSYC/qNjo+btzmzFQ==5tVlQhx4ODrER2iv' })
         @results = JSON.parse(response.body)
     end
 end

1e316367413548481919dc8df6476e117d5793e0
Elijah2609 <56544041+Elijah2609@users.noreply.github.com>
2022-12-07 19:01:34 +0000

    update

--- app/jobs/example_job.rb
+++ app/jobs/example_job.rb
@@ -1,8 +1,7 @@
 class ExampleJob < ApplicationJob
-queue_as: default
-
-def perform( * args)
-  sleep 5
-  logger.info "Just waited 5 seconds"
+# queue_as: default
+  def perform( * args)
+    sleep 5
+    logger.info "Just waited 5 seconds"
   end
 end

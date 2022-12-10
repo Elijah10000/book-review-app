7756dd091e08b49dec2a67f255fd7711702b8f79
Elijah2609 <56544041+Elijah2609@users.noreply.github.com>
2022-12-10 14:30:16 +0000

    testing

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,29 +1,45 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'test_helper'
+require 'test_helper'
 
-# class SessionControllerTest < ActionDispatch::IntegrationTest
-#   test 'should get index' do
-#     get session_index_path(@session)
-#     assert_response :success
-#   end
+class SessionControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get session_index_path(@session)
+    assert_response :success
+  end
 
-#   test 'should get ping' do
-#     get session_ping_path(@session)
-#     assert_response :success
-#   end
+  test 'should get ping' do
+    get session_ping_path(@session)
+    assert_response :success
+  end
 
-#   test 'should get pong' do
-#     get session_pong_path(@session)
-#     assert_response :success
-#   end
+  test 'should get pong' do
+    get session_pong_path(@session)
+    assert_response :success
+  end
 
-#  test 'should get request' do
-#     get session_request_path(@session)
-#     assert_response :success
-#   end
-# end
+ test 'should get request' do
+    get session_request_path(@session)
+    assert_response :success
+  end
 
-# # Path: book-review-app\test\controllers\session_controller_test.rb
-# # Compare this snippet from book-review-app\test\controllers\reviews_controller_test.rb:
-# # # frozen_string_literal: true
+    test 'should get response' do
+    get session_response_path(@session)
+    assert_response :success
+    end
+
+    test 'should get flash' do
+    get session_flash_path(@session)
+    assert_response :success
+    end
+
+    test 'should get flash_now' do
+    get session_flash_now_path(@session)
+    assert_response :success
+    end
+
+    test 'should get redirect' do
+    get session_redirect_path(@session)
+    assert_response :success
+    end 
+end

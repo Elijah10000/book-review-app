e393bd76d485c8071bc54b93b04ff208cf619d1e
Elijah McNamara <78796236+Elijah10000@users.noreply.github.com>
2022-12-10 14:35:46 +0000

    Revert "Testing updates and fixes"

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,35 +1,45 @@
 # # frozen_string_literal: true
 
- require 'test_helper'
+# require 'test_helper'
 
- class BooksControllerTest < ActionDispatch::IntegrationTest
-  setup do 
-   @book = books.(:one)
- end
+# class BooksControllerTest < ActionDispatch::IntegrationTest
+#  setup do 
+#   @book = books.(:one)
+# end
 
- test "should get index" do
-    get books_url
-    assert_response :success
- end
+# test "should get index" do
+#   get books_url
+#   assert_response :success
+# end
 
-    test "should get new" do
-    get new_book_url
-    assert_response :success
-end
+# test "should show book" do
+#   get new_book_url
+#   assert_response :success
+# end
 
-    test "should create book" do
-    assert_difference('Book.count')
-end
-
-    test "should show book" do
-    get book_url(@book)
-    assert_response :success
-    end
-
-    test "should get edit" do
-    get edit_book_url(@book)
-    assert_response :success
-    end 
-end
+# test "should get new" do
+#   get new_book_path(@book)
+#   assert_response :success
+# end
 
 
+# test "should create book" do 
+#   get new_book_url
+#   assert_response :success
+# end
+
+# test "should update book" do
+#   get edit_book_path(@book)
+#   assert_response :success
+# end
+
+# test "should destroy book" do
+#   get destroy_book_path(@book)
+#   assert_response :success
+# end
+
+# end
+
+# # Path: book-review-app\test\fixtures\books.yml
+# # Compare this snippet from book-review-app\app\models\book.rb:
+# # # frozen_string_literal: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,68 +3,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    user = User.new
-  end
-
-  test "Should test if a user has a name" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has an email" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a password" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a password confirmation" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation and a book" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation and a book and a category" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation and a book and a category and a review" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation and a book and a category and a review and a comment" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "Should test if a user has a name and an email and a password and a password confirmation and a book and a category and a review and a comment and a rating" do
-    user = User.new
-    assert_not user.save
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end

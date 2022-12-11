05188acf1d4c37c74da0c6c7423267bf6e10a6f8
Elijah2609 <56544041+Elijah2609@users.noreply.github.com>
2022-12-11 13:47:54 +0000

    fixes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
       @books = Book.all.order('created_at DESC')
     else
       @category_id = Category.find_by(name: params[:category]).id
-      @books = Book.where(category_id: @category_id).order('created_at DESC')
+      @books = Book.where(category_id => @category_id).order('created_at DESC')
     end
   end
 
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @categories = Category.all.map { |c| [c.name, c.id] }
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def update
